b086f6fa7e5f3855bfd954c3dee027571f7540cd
Gordon Gao <gordonsdev@gmail.com>
2016-10-26 20:15:59 -0400

    init

---
+++ GGValidator.swift
@@ -0,0 +1,52 @@
+//
+//  PLValidator.swift
+//  PLCodeTest
+//
+//  Created by GaoYuan on 10/8/16.
+//  Copyright © 2016 YuanGao. All rights reserved.
+//
+
+
+enum PLValidator {
+  case required
+}
+
+extension PLValidator {
+  func isValid(_ attrs: Set<String>) -> Bool {
+    switch self {
+    case .required:
+      let length = attrs.filter({ attr in attr == "" }).count
+      return length != 0 ? false : true
+    }
+  }
+  
+  func isValidWithArray(_ a: Set<String>, _ block: (String) -> Bool) -> Bool {
+    switch self {
+    case .required:
+      var flag = true
+      
+      for e in a {
+        flag = block(e)
+      }
+      
+      return flag
+    }
+  }
+  
+  func isValidGeneric<T: Equatable>(_ a: Set<T>, _ checker: T) -> Bool {
+    switch self {
+    case .required:
+      var flag = true
+      
+      for e in a {
+        flag = e == checker
+      }
+      
+      return flag
+    }
+  }
+  
+  
+  
+
+}
